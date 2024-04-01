43c5823386a684f9a000f23463351c6a4def7c9d
HaoZhengZhao <haozheng.zhao@poper.co>
2024-04-01 22:06:15 +0800

    fix: fixed command

--- controllers/command.go
+++ controllers/command.go
@@ -36,21 +36,21 @@ func (ctrl *CommandController) GitPull() {
 func (ctrl *CommandController) Start() {
 	commend_start, _ := config.String("web::commend_start")
 	cmd := exec.Command("bash", "-c", commend_start)
+	cmd.Start() // 异步执行，不要获取结果，因为重启时此进程会挂掉导致http请求失败
 	
-	// 获取标准输出
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		ctrl.Ctx.Resp(map[string]interface{} {
-			"code": 400,
-			"data": err,
-			"msg": "error",
-		})
-	}
+	// // 获取标准输出
+	// output, err := cmd.CombinedOutput()
+	// if err != nil {
+	// 	ctrl.Ctx.Resp(map[string]interface{} {
+	// 		"code": 400,
+	// 		"data": err,
+	// 		"msg": "error",
+	// 	})
+	// }
 	
 	ctrl.Ctx.Resp(map[string]interface{} {
 		"code": 200,
 		"msg": "success",
-		"data": string(output),
 	})
 }
 
@@ -58,21 +58,21 @@ func (ctrl *CommandController) Start() {
 func (ctrl *CommandController) Stop() {
 	commend_stop, _ := config.String("web::commend_stop")
 	cmd := exec.Command("bash", "-c", commend_stop)
+	cmd.Start() // 异步执行，不要获取结果，因为重启时此进程会挂掉导致http请求失败
 	
-	// 获取标准输出
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		ctrl.Ctx.Resp(map[string]interface{} {
-			"code": 400,
-			"data": err,
-			"msg": "error",
-		})
-	}
+	// // 获取标准输出
+	// output, err := cmd.CombinedOutput()
+	// if err != nil {
+	// 	ctrl.Ctx.Resp(map[string]interface{} {
+	// 		"code": 400,
+	// 		"data": err,
+	// 		"msg": "error",
+	// 	})
+	// }
 	
 	ctrl.Ctx.Resp(map[string]interface{} {
 		"code": 200,
 		"msg": "success",
-		"data": string(output),
 	})
 }
 
