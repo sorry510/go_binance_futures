19d3fd11448b913192ff9f42e21f12c2751c6e08
HaoZhengZhao <haozheng.zhao@poper.co>
2024-04-01 23:07:44 +0800

    fix: fixed bug

--- controllers/command.go
+++ controllers/command.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"io"
 	"os/exec"
 
 	"github.com/beego/beego/v2/core/config"
@@ -78,8 +79,43 @@ func (ctrl *CommandController) Stop() {
 
 // pm2-log
 func (ctrl *CommandController) Pm2Log() {
-	ctrl.Ctx.Resp(map[string]interface{} {
-		"code": 200,
-		"msg": "success",
-	})
+	commend_log, _ := config.String("web::commend_log")
+	paramsKey := ctrl.GetString("key")
+	if paramsKey != "sorry510" {
+		ctrl.Ctx.Resp(map[string]interface{} {
+			"code": 400,
+			"msg": "error",
+		})
+		return
+	}
+	
+	cmd := exec.Command("bash", "-c", commend_log)
+	
+	// 创建标准输出管道，用于读取命令的输出
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		ctrl.Ctx.Resp(map[string]interface{} {
+			"code": 400,
+			"msg": err,
+		})
+		return
+	}
+	
+	ctrl.Ctx.ResponseWriter.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	// 开始执行命令
+	if err := cmd.Start(); err != nil {
+		ctrl.Ctx.Resp(map[string]interface{} {
+			"code": 400,
+			"msg": err,
+		})
+		return
+	}
+	// 直接从命令输出流读取数据并写入响应
+	if _, err := io.Copy(ctrl.Ctx.ResponseWriter, stdout); err != nil {
+		ctrl.Ctx.Resp(map[string]interface{} {
+			"code": 400,
+			"msg": err,
+		})
+		return
+	}
 }
