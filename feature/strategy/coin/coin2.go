bbad064550a41c99a4dfe51ebe7edd77cfb9d8d9
HaoZhengZhao <haozheng.zhao@poper.co>
2024-04-10 21:02:30 +0800

    fix: fixed coin2

--- feature/strategy/coin/coin2.go
+++ feature/strategy/coin/coin2.go
@@ -8,7 +8,7 @@ import (
 type TradeCoin2 struct {
 }
 
-// 策略: 从涨幅榜中前6个里面随机选取2个，从跌幅榜中的前6个里面随机选取2个, 最近5min交易过的币不再交易
+// 策略: 最近5min交易过的币不再交易,随机选取3个
 func (tradeCoin1 TradeCoin2) SelectCoins(allCoins []*models.Symbols) (coins []*models.Symbols) {
 	exclude_symbols_map := getLimitMinOrder(5)
 	sort.SliceStable(allCoins, func(i, j int) bool {
@@ -24,13 +24,7 @@ func (tradeCoin1 TradeCoin2) SelectCoins(allCoins []*models.Symbols) (coins []*m
 			filterCoins = append(filterCoins, coin)
 		}
 	}
-	sliceLength := 6
-	if len(filterCoins) < sliceLength {
-		sliceLength = len(filterCoins)
-	}
-	res1 := GetRandArr(filterCoins[:sliceLength], 2)
-	res2 := GetRandArr(filterCoins[len(filterCoins) - sliceLength:], 2)
-	coins = append(coins, res1...)
-	coins = append(coins, res2...)
+
+	coins = GetRandArr(filterCoins, 3)
 	return coins
 }
